c6c8b14389d1d0c858beb9d4d5713299007d7f7b
Taja House <ahlliahtaja@yahoo.com>
2020-05-30 09:34:40 -0700

    Setting up fetch in actions and reducers

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { FETCH_SMURF_START, FETCH_SMURF_SUCCESS, FETCH_SMURF_FAILURE} from "../actions/index"
+
 const initialState = {
     smurf:[],
     isFetching: false,
@@ -5,7 +7,31 @@ const initialState = {
 };
 
 const reducer = (state =initialState, action) =>{
+    switch(action.type){
+        //fetch
+        case FETCH_SMURF_START:
+            return{
+                ...state,
+                smurf:[],
+                isFetching:true,
+                error:''
+            }
+        case FETCH_SMURF_SUCCESS:
+            return {
+                ...state, 
+                smurf:action.payload,
+                isFetching:false,
+                error:''
+            }
+        case FETCH_SMURF_FAILURE:
+            return{
+                ...state,
+                smurf:[],
+                isFetching:false,
+                error:action.payload
+            }
+        default: return state
 
-   
+        }   
 }
 export default reducer;
